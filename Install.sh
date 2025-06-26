b66f9d9eb9ad118587f1f39428f6d371bb9b8df9
Kenny <wanfengyun@vip.qq.com>
2025-06-26 15:18:24 +0800

    Update Install.sh
    test

--- Install.sh
+++ Install.sh
@@ -199,6 +199,10 @@ while getopts "u:p:c:q:l:s:rbvxyz3ohf" opt; do
 				fi
 			done
 		fi
+
+		# 您放置的调试行在这里，这是正确的位置
+		echo "DEBUG: vertex_install 变量状态: '$vertex_install'" 
+
 		if [[ -n "$vertex_install" ]]; then
 			need_input "Please enter vertex port:"
 			read vertex_port
@@ -213,7 +217,7 @@ while getopts "u:p:c:q:l:s:rbvxyz3ohf" opt; do
 				fi
 			done
 		fi
-		if [[ -n "$fb_install" ]]; then # 新增 Filebrowser 端口逻辑
+		if [[ -n "$fb_install" ]]; then # Filebrowser 端口逻辑
 			need_input "Please enter Filebrowser port:"
 			read fb_port
 			while true
